3b7559c4ce025dba7328fc1b7b909fb1f445da8e
Eduardo Moreira <96441062+EduMoreiira@users.noreply.github.com>
2024-02-27 22:24:00 -0300

    Add files via upload

---
+++ LOGICA 2/ATIVIDADE 2/ATIV02_03.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atividade02;
+
+/**
+ *
+ * @author eduu_
+ */
+public class ATIV02_03 {
+    public static void main(String[] args){
+        
+        int quantiaEmDolares = 4;
+        float cotacaoDoDolar = 4.6f;
+        float quantiaEmReais = quantiaEmDolares * cotacaoDoDolar;
+        
+        System.out.println("Valor da conversão: "+quantiaEmReais);
+    }
+}
---
+++ LOGICA 2/ATIVIDADE 2/ATIV02_04.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.atividade02;
+
+/**
+ *
+ * @author eduu_
+ */
+public class ATIV02_04 {
+    public static void main(String[] args){
+        float distancia = 5.6f;
+        float litros = 3.5f;
+        
+        float taxaDeConsumo = distancia / litros;   
+        
+        System.out.println("Taxa de consumo: "+taxaDeConsumo+" litros por kilometro");
+    }
+}
