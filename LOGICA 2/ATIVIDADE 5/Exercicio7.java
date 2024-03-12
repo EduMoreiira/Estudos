251a523360b923de2c3bd795b6b70a934c546502
Eduardo Moreira <dudu_moreira9@hotmail.com>
2024-03-12 11:23:34 -0300

    exercicio7

--- LOGICA 2/ATIVIDADE 5/Exercicio7.java
+++ LOGICA 2/ATIVIDADE 5/Exercicio7.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Exercicio7 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int candidato1 = 0, candidato2 = 0, candidato3 = 0, candidato4 = 0, nulo = 0;
+        int candidato1 = 0, candidato2 = 0, candidato3 = 0, candidato4 = 0, nulo = 0, branco = 0;
         
         for(;;){
             System.out.println("Insira um digito para confirmar seu voto:");
@@ -18,18 +18,44 @@ public class Exercicio7 {
 
             if(resposta == 1){
                 candidato1++;
+                System.out.println("Você votou no candidato [1]!");
+
             }else if(resposta == 2){
                 candidato2++;
-            
-            }else if(resposta == 2){
-                candidato2++;
+                System.out.println("Você votou no candidato [2]!");
             
             }else if(resposta == 3){
                 candidato3++;
+                System.out.println("Você votou no candidato [3]!");
             
             }else if(resposta == 4){
-                candidato4++;
+                candidato3++;
+                System.out.println("Você votou no candidato [4]!");
+
+            }else if(resposta == 5){
+                branco++;
+                System.out.println("Você votou [Em Branco]!");
+
+            }else if(resposta == 0){
+                int totalVotos = candidato1 + candidato2 + candidato3 + candidato4 + branco + nulo;
+                double porcentagemNulo = ((double)nulo / (double)totalVotos) * 100;
+                double porcentagemBranco = ((double)branco / (double)totalVotos) * 100;
+
+                System.out.println("Total de votos no Candidato[1]: " + candidato1);
+                System.out.println("Total de votos no Candidato[2]: " + candidato2);
+                System.out.println("Total de votos no Candidato[3]: " + candidato3);
+                System.out.println("Total de votos no Candidato[4]: " + candidato4);
+                System.out.println("Total de votos [Em branco]: " + branco);
+                System.out.println("Total de votos [Nulos]: " + nulo);
+                System.out.println("Porcentagem de votos [Nulos] sobre o total: " + String.format("%.2f",porcentagemNulo) + "%");
+                System.out.println("Porcentagem de votos [Em Branco] sobre o total: " + String.format("%.2f",porcentagemBranco) + "%");
+                break;
+            
+            }else{
+                nulo++;
+                System.out.println("Você votou [Nulo]!");
             }
+            
         }
     }
 }
